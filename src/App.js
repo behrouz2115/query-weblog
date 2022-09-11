11f443483dadb2e2973b51dbe4a8b66798ea38f8
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2022-09-11 17:30:07 +0900

    layout finidhed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import HomePage from './components/home/HomePage';
-import Header from './components/layout/Header';
+import Layout from './components/layout/Index';
 
 
 
@@ -8,8 +8,9 @@ function App() {
 
   return (
     < >
-       <Header/>
+       <Layout>
        <HomePage/>
+       </Layout>
     </>
   );
 }
---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+const Footer = () => {
+    return (
+        <footer>
+            <Typography component="p" variant='p' bgColor='#f7f7f7' color="primary" padding='10px' textAlign='center' mt={10}>
+                behrouz.rozmeh@gmial.com
+            </Typography>
+        </footer>
+    );
+};
+
+export default Footer;
