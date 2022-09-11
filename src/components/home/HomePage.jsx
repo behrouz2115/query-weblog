e71364b96e71de896b334676b23093a5d1f6ebdf
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2022-09-11 16:14:20 +0900

    homepage is updated

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -1,13 +1,23 @@
+import { Container, Grid, Typography } from '@mui/material';
 import React from 'react';
 import Author from '../author/Author';
 import Blog from '../blog/Blog';
 
 const HomePage = () => {
     return (
-        <div>
-            <Blog/>
-            <Author/>
-        </div>
+        <Container maxWidth='lg'>
+            <Grid container spacing={2} p={4}>
+                <Grid item xs={12} md={3} mt={4}>
+                    <Typography component='h3' variant='h5' mb={3} fontWeight={700}>Authors</Typography>
+                    <Author/>
+                </Grid>
+                <Grid item xs={12} md={9} mt={4}>
+                <Typography component='h3' variant='h5' mb={3} fontWeight={700}>Posts</Typography>
+                <Blog/>
+                </Grid>
+            </Grid>
+
+        </Container>
     );
 };
 
