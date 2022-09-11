e158109153270eef3d01cfe14105a71f5cb3c180
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2022-09-11 15:38:04 +0900

    .env made

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 import App from './App';
 
 const client = new ApolloClient({
-    uri:" https://api-ap-northeast-1.hygraph.com/v2/cl7t1t1xa0fa101tf0k863kah/master  ",
+    uri:  process.env.REACT_APP_GRAPHCMS_URI,
     cache: new InMemoryCache(),
 });
 
