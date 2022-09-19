f08a285543d6cfceaf8bdf229134ce78447cb394
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2022-09-19 21:41:26 +0900

    author page updated

--- src/components/author/AuthorPage.jsx
+++ src/components/author/AuthorPage.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
 import { useParams } from 'react-router-dom';
+import { GET_AUTHOR_INFO } from '../../graphql/quaries';
 
 const AuthorPage = () => {
-    const params = useParams();
-    console.log(params)
+    const { slug } = useParams();
+    const { loading, data, errors } = useQuery(GET_AUTHOR_INFO, {
+        variables:{ slug }
+    });
+    if (loading) return <h4>Loading ...</h4>
+    if (errors) return <h4>error();</h4>
+    console.log(data)
     return (
         <div>
             author page
