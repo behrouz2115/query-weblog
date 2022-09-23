6b75118d22f5c5b891ea0b6da687c6b072ab6ae9
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2022-09-23 23:42:55 +0900

    author page design

--- src/components/author/AuthorPage.jsx
+++ src/components/author/AuthorPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useQuery } from '@apollo/client';
 import { useParams } from 'react-router-dom';
 import { GET_AUTHOR_INFO } from '../../graphql/quaries';
+import { Avatar, Container, Grid, Typography } from '@mui/material'
 
 const AuthorPage = () => {
     const { slug } = useParams();
@@ -9,12 +10,19 @@ const AuthorPage = () => {
         variables:{ slug }
     });
     if (loading) return <h4>Loading ...</h4>
-    if (errors) return <h4>error();</h4>
+    if (errors) return <h4>error</h4>
     console.log(data)
     return (
-        <div>
-            author page
-        </div>
+        <Container maxWidth='lg'>
+            <Grid container mt={10}>
+                <Grid item  xs={12} display='flex' flexDirection='column' alignItems='center'>
+                    <Avatar src={data.author.avatar.url} sx={{width:250, height:250}} />
+                    <Typography component='h3' variant='h6' fontWeight={700} mt={4}>{data.author.name}</Typography>
+                    <Typography component='p' variant='h6' color='text.secondary' mt={2}>{data.author.field}</Typography>
+                </Grid>
+            </Grid>
+
+        </Container>
     );
 };
 
