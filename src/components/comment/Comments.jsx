aeac7decccf335c76a6209cd5b5a88376e6766a0
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2022-09-29 19:16:06 +0900

    comment shows

--- src/components/comment/Comments.jsx
+++ src/components/comment/Comments.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 import { GET_POST_COMMENT } from '../../graphql/quaries';
+import { Avatar, Box, Grid, Typography } from '@mui/material';
 
 const Comments = ({slug}) => {
     const { loading, data, errors } =useQuery(GET_POST_COMMENT, {variables:{slug}});
     console.log(data)
     return (
-        <div>
-            comment
-        </div>
+        <Grid container  sx={{
+            boxShadow: "rgba(0,0,0,0.1) 0px 4px 12px",
+            borderRadius: 4,
+            py: 1,
+            mt: 5,
+          }}>
+            <Grid item sx={12} m={2}>
+                <Typography container='p' variant='h6' fontWeight={700} color='primary'>Comments</Typography>
+                {data.comments.map(comment => (
+                    <Grid item xs={12} key={comment.id} m={2} p={2} border='1px silver solid' borderRadius={1}>
+                        <Box component='div'>
+                            <Avatar>{comment.name[0]}</Avatar>
+                        </Box>
+                    </Grid>
+                ))}
+            </Grid>
+        </Grid>
     );
 };
 
