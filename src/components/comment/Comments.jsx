9260e06deb3a2f9251d23b357171f70560f6a49a
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2022-09-29 19:28:22 +0900

    comment updated

--- src/components/comment/Comments.jsx
+++ src/components/comment/Comments.jsx
@@ -5,6 +5,7 @@ import { Avatar, Box, Grid, Typography } from '@mui/material';
 
 const Comments = ({slug}) => {
     const { loading, data, errors } =useQuery(GET_POST_COMMENT, {variables:{slug}});
+    if (loading) return null;
     console.log(data)
     return (
         <Grid container  sx={{
@@ -13,13 +14,19 @@ const Comments = ({slug}) => {
             py: 1,
             mt: 5,
           }}>
-            <Grid item sx={12} m={2}>
+            <Grid item xs={12} m={2}>
                 <Typography container='p' variant='h6' fontWeight={700} color='primary'>Comments</Typography>
                 {data.comments.map(comment => (
                     <Grid item xs={12} key={comment.id} m={2} p={2} border='1px silver solid' borderRadius={1}>
-                        <Box component='div'>
+                        <Box component='div' display='flex' alignItems='center' mb={3}>
                             <Avatar>{comment.name[0]}</Avatar>
+                            <Typography component='h5' variant='p' fontWeight={700} ml={1}>
+                                {comment.name}
+                            </Typography>
                         </Box>
+                        <Typography component='p' variant='p'>
+                            {comment.text}
+                        </Typography>
                     </Grid>
                 ))}
             </Grid>
