50bec8f3e7e7fea7413e3f18d7c433340881ae0a
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2022-10-04 21:54:26 +0900

    delete cosole.log

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -7,7 +7,6 @@ import Loader from '../shared/Loader';
 
 const Blog = () => {
     const { loading, data, errors } = useQuery(GET_BLOGS_INFO);
-    console.log (data)
     if (loading) return <Loader/>;
     if (errors) return <h4>error();</h4>
 
