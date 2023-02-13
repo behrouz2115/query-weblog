1490bddb0c7ae680993e81c98de21b05912924ff
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2023-02-13 23:02:57 +0900

    small change

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer>
             <Typography component="p" variant='p' bgcolor='#f7f7f7' color="primary" padding='10px' textAlign='center' mt={10}>
-                behrouz.rozmeh@gmial.com
+                behrouz.rozmeh@gmail.com
             </Typography>
         </footer>
     );
